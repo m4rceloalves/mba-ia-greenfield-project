aa5746b2ff33010dbe59b6acd87e2d8313635afd
Luiz Carlos <argentinaluiz@gmail.com>
2026-05-10 20:01:56 -0300

    ajustes no globals.css pós instalação do shadcn

--- next-frontend/app/layout.tsx
+++ next-frontend/app/layout.tsx
@@ -1,9 +1,7 @@
 import type { Metadata } from "next";
-import { Inter, Geist_Mono, Geist } from "next/font/google";
+import { Inter, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
-
-const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 const inter = Inter({
   variable: "--font-inter",
@@ -29,7 +27,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={cn("h-full", "antialiased", inter.variable, geistMono.variable, "font-sans", geist.variable)}
+      className={cn("h-full", "antialiased", inter.variable, geistMono.variable, "font-sans")}
     >
       <body className="min-h-full flex flex-col font-sans">{children}</body>
     </html>
